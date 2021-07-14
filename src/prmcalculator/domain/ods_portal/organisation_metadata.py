4e3f4a79bf150c40654590e98fae78e2657b5fb0
Dorota-TW <dorota.sobkow1@nhs.net>
2021-07-14 16:56:59 +0100

    PRMT-2109 [KV/DS] Add OrganisationMetadata dataclass

--- src/prmcalculator/domain/ods_portal/organisation_metadata.py
+++ src/prmcalculator/domain/ods_portal/organisation_metadata.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from datetime import datetime
+from dateutil import parser
 from typing import List
 
 
@@ -14,3 +16,26 @@ class CcgDetails:
     ods_code: str
     name: str
     practices: List[str]
+
+
+@dataclass
+class OrganisationMetadata:
+    generated_on: datetime
+    practices: List[PracticeDetails]
+    ccgs: List[CcgDetails]
+
+    @classmethod
+    def from_dict(cls, data):
+        return OrganisationMetadata(
+            generated_on=parser.isoparse(data["generated_on"]),
+            practices=[
+                PracticeDetails(
+                    asids=practice["asids"], ods_code=practice["ods_code"], name=practice["name"]
+                )
+                for practice in data["practices"]
+            ],
+            ccgs=[
+                CcgDetails(ods_code=ccg["ods_code"], name=ccg["name"], practices=ccg["practices"])
+                for ccg in data["ccgs"]
+            ],
+        )
