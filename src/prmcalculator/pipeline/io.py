f67789374c2fecfd93acdc0f57bb7537b297d996
K-Varsani <kavita.varsani1@nhs.net>
2021-07-15 10:38:14 +0100

    PRMT-2109 [DS/KV] Add reading from and writing json files to s3 bucket

---
+++ src/prmcalculator/pipeline/io.py
@@ -0,0 +1,70 @@
+from dataclasses import asdict
+
+from prmcalculator.domain.practice.metrics_presentation import PracticeMetricsPresentation
+from prmcalculator.domain.ods_portal.organisation_metadata import OrganisationMetadata
+from prmcalculator.utils.io.dictionary import camelize_dict
+from prmcalculator.utils.reporting_window import MonthlyReportingWindow
+from prmcalculator.utils.io.s3 import S3DataManager
+
+
+class PlatformMetricsIO:
+    _ORG_METADATA_VERSION = "v2"
+    _ORG_METADATA_FILE_NAME = "organisationMetadata.json"
+    _DASHBOARD_DATA_VERSION = "v3"
+    _PRACTICE_METRICS_FILE_NAME = "practiceMetrics.json"
+
+    def __init__(
+        self,
+        *,
+        reporting_window: MonthlyReportingWindow,
+        s3_data_manager: S3DataManager,
+        organisation_metadata_bucket: str,
+        dashboard_data_bucket: str,
+    ):
+        self._window = reporting_window
+        self._s3_manager = s3_data_manager
+        self._org_metadata_bucket_name = organisation_metadata_bucket
+        self._dashboard_data_bucket = dashboard_data_bucket
+
+    def _dashboard_data_bucket_s3_path(self, file_name: str) -> str:
+        return "/".join(
+            [
+                self._dashboard_data_bucket,
+                self._DASHBOARD_DATA_VERSION,
+                self._metric_month_path_fragment(),
+                file_name,
+            ]
+        )
+
+    @staticmethod
+    def _create_platform_json_object(platform_data) -> dict:
+        content_dict = asdict(platform_data)
+        return camelize_dict(content_dict)
+
+    def _metric_month_path_fragment(self) -> str:
+        return f"{self._window.metric_year}/{self._window.metric_month}"
+
+    def _overflow_month_path_fragment(self) -> str:
+        return f"{self._window.overflow_year}/{self._window.overflow_month}"
+
+    def read_ods_metadata(self) -> OrganisationMetadata:
+        ods_metadata_s3_path = "/".join(
+            [
+                self._org_metadata_bucket_name,
+                self._ORG_METADATA_VERSION,
+                self._overflow_month_path_fragment(),
+                self._ORG_METADATA_FILE_NAME,
+            ]
+        )
+
+        ods_metadata_dict = self._s3_manager.read_json(f"s3://{ods_metadata_s3_path}")
+        return OrganisationMetadata.from_dict(ods_metadata_dict)
+
+    def write_practice_metrics(self, practice_metrics: PracticeMetricsPresentation):
+        practice_metrics_path = self._dashboard_data_bucket_s3_path(
+            self._PRACTICE_METRICS_FILE_NAME
+        )
+        self._s3_manager.write_json(
+            f"s3://{practice_metrics_path}",
+            self._create_platform_json_object(practice_metrics),
+        )
---
+++ tests/unit/utils/test_reporting_window.py
@@ -0,0 +1,98 @@
+from datetime import datetime
+
+import pytest
+from dateutil.tz import tzutc
+
+from prmcalculator.utils.reporting_window import MonthlyReportingWindow
+
+
+def test_prior_to_correctly_determines_metric_month():
+    moment = datetime(2021, 3, 4)
+
+    reporting_window = MonthlyReportingWindow.prior_to(moment)
+
+    expected = 2
+
+    actual = reporting_window.metric_month
+
+    assert actual == expected
+
+
+def test_prior_to_correctly_determines_metric_year():
+    moment = datetime(2021, 3, 4)
+
+    reporting_window = MonthlyReportingWindow.prior_to(moment)
+
+    expected = 2021
+
+    actual = reporting_window.metric_year
+
+    assert actual == expected
+
+
+def test_prior_to_correctly_determines_metric_month_over_new_year():
+    moment = datetime(2021, 1, 4)
+
+    reporting_window = MonthlyReportingWindow.prior_to(moment)
+
+    expected = 12
+
+    actual = reporting_window.metric_month
+
+    assert actual == expected
+
+
+def test_prior_to_correctly_determines_metric_year_over_new_year():
+    moment = datetime(2021, 1, 4)
+
+    reporting_window = MonthlyReportingWindow.prior_to(moment)
+
+    expected = 2020
+
+    actual = reporting_window.metric_year
+
+    assert actual == expected
+
+
+def test_prior_to_correctly_determines_overflow_month():
+    moment = datetime(2021, 3, 4)
+
+    reporting_window = MonthlyReportingWindow.prior_to(moment)
+
+    expected = 3
+
+    actual = reporting_window.overflow_month
+
+    assert actual == expected
+
+
+def test_prior_to_correctly_determines_overflow_year():
+    moment = datetime(2021, 3, 4)
+
+    reporting_window = MonthlyReportingWindow.prior_to(moment)
+
+    expected = 2021
+
+    actual = reporting_window.overflow_year
+
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        ({"date": datetime(2021, 1, 31, tzinfo=tzutc()), "expected": False}),
+        ({"date": datetime(2021, 2, 1, tzinfo=tzutc()), "expected": True}),
+        ({"date": datetime(2021, 2, 20, tzinfo=tzutc()), "expected": True}),
+        ({"date": datetime(2021, 2, 28, tzinfo=tzutc()), "expected": True}),
+        ({"date": datetime(2021, 3, 1, tzinfo=tzutc()), "expected": False}),
+    ],
+)
+def test_contains_returns_correct_boolean(test_case):
+    moment = datetime(2021, 3, 4)
+
+    reporting_window = MonthlyReportingWindow.prior_to(moment)
+
+    actual = reporting_window.contains(test_case["date"])
+
+    assert actual == test_case["expected"]
