604d35a33349e9078d3854de06304e9a030096ac
K-Varsani <kavita.varsani1@nhs.net>
2021-07-21 14:10:39 +0100

    PRMT-2109 [HZ/KV] Produce and write practice metrics json file to s3

--- src/prmcalculator/pipeline/main.py
+++ src/prmcalculator/pipeline/main.py
@@ -2,6 +2,7 @@ from os import environ
 import boto3
 
 from prmcalculator.pipeline.config import PipelineConfig
+from prmcalculator.pipeline.core import calculate_practice_metrics_data
 from prmcalculator.pipeline.io import PlatformMetricsIO
 from prmcalculator.utils.io.s3 import S3DataManager
 from prmcalculator.utils.reporting_window import MonthlyReportingWindow
@@ -15,10 +16,20 @@ def main():
 
     reporting_window = MonthlyReportingWindow.prior_to(config.date_anchor)
 
-    PlatformMetricsIO(
+    metrics_io = PlatformMetricsIO(
         reporting_window=reporting_window,
         s3_data_manager=s3_manager,
         organisation_metadata_bucket=config.organisation_metadata_bucket,
         transfer_data_bucket=config.input_transfer_data_bucket,
         data_platform_metrics_bucket=config.output_metrics_bucket,
     )
+
+    organisation_metadata = metrics_io.read_ods_metadata()
+
+    transfers = metrics_io.read_transfer_data()
+
+    practice_metrics_data = calculate_practice_metrics_data(
+        transfers, organisation_metadata, reporting_window
+    )
+
+    metrics_io.write_practice_metrics(practice_metrics_data)
