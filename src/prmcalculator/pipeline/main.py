27311f2e7b586b3217d49869925927636f2a1bbf
helen.zhou <helen.zhou1@nhs.net>
2021-07-28 15:45:46 +0100

    PRMT-2109 [HZ/JN] Add logging to enable debugging

--- src/prmcalculator/pipeline/main.py
+++ src/prmcalculator/pipeline/main.py
@@ -1,5 +1,6 @@
 from os import environ
 import boto3
+import logging
 
 from prmcalculator.pipeline.config import PipelineConfig
 from prmcalculator.pipeline.core import (
@@ -10,14 +11,21 @@ from prmcalculator.pipeline.io import PlatformMetricsIO
 from prmcalculator.utils.io.s3 import S3DataManager
 from prmcalculator.utils.reporting_window import MonthlyReportingWindow
 
+logger = logging.getLogger(__name__)
+
 
 def main():
+    logger.info("Start to read environment variables from config")
     config = PipelineConfig.from_environment_variables(environ)
 
     s3 = boto3.resource("s3", endpoint_url=config.s3_endpoint_url)
     s3_manager = S3DataManager(s3)
 
     reporting_window = MonthlyReportingWindow.prior_to(config.date_anchor)
+    logger.info(
+        f"""Reporting Window: {reporting_window.metric_year}/{reporting_window.metric_month},
+        overflow: {reporting_window.overflow_year}/{reporting_window.overflow_month}"""
+    )
 
     metrics_io = PlatformMetricsIO(
         reporting_window=reporting_window,
@@ -27,16 +35,22 @@ def main():
         data_platform_metrics_bucket=config.output_metrics_bucket,
     )
 
+    logger.info(f"Reading from  s3://{config.organisation_metadata_bucket}")
     organisation_metadata = metrics_io.read_ods_metadata()
 
+    logger.info(f"Reading from  s3://{config.input_transfer_data_bucket}")
     transfers = metrics_io.read_transfer_data()
 
+    logger.info("Calculating practice metrics")
     practice_metrics_data = calculate_practice_metrics_data(
         transfers, organisation_metadata, reporting_window
     )
+
+    logger.info("Calculating national metrics")
     national_metrics_data = calculate_national_metrics_data(
         transfers=transfers, reporting_window=reporting_window
     )
 
+    logger.info(f"Attempting to write to S3 bucket {config.output_metrics_bucket}")
     metrics_io.write_practice_metrics(practice_metrics_data)
     metrics_io.write_national_metrics(national_metrics_data)
