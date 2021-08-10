7a75d2e414191edc43ba2fee4ca468de3a36b550
Jake Newman <jacob.newman1@nhs.net>
2021-08-10 16:14:55 +0100

    PRMT-2234 [JN/HZ] Count by failure reason method in TransferOutcomeCounter

--- src/prmcalculator/domain/national/transfer_outcome_counter.py
+++ src/prmcalculator/domain/national/transfer_outcome_counter.py
@@ -1,14 +1,21 @@
 from typing import List
 
-from prmcalculator.domain.gp2gp.transfer import Transfer
+from prmcalculator.domain.gp2gp.transfer import Transfer, TransferFailureReason
 
 
 class TransferOutcomeCounter:
-    _transfers: List[Transfer] = []
+    def __init__(self):
+        self._transfers: List[Transfer] = []
 
     @property
     def total(self) -> int:
         return len(self._transfers)
+
+    def count_by_failure_reason(self, failure_reason: TransferFailureReason) -> int:
+        transfers_with_failure_reason = [
+            transfer.outcome.failure_reason == failure_reason for transfer in self._transfers
+        ]
+        return sum(transfers_with_failure_reason)
 
     def add_transfer(self, transfer: Transfer):
         self._transfers.append(transfer)
--- tests/unit/domain/national/test_transfer_outcome_counter.py
+++ tests/unit/domain/national/test_transfer_outcome_counter.py
@@ -1,5 +1,11 @@
+from prmcalculator.domain.gp2gp.transfer import TransferFailureReason
 from prmcalculator.domain.national.transfer_outcome_counter import TransferOutcomeCounter
-from tests.builders.gp2gp import build_transfer
+from tests.builders.gp2gp import (
+    build_transfer,
+    a_transfer_integrated_beyond_8_days,
+    a_transfer_that_was_never_integrated,
+    a_transfer_with_a_final_error,
+)
 
 
 def test_returns_total_count_of_transfers():
@@ -11,5 +17,41 @@ def test_returns_total_count_of_transfers():
 
     actual = counter.total
     expected = 2
+
+    assert actual == expected
+
+
+def test_returns_total_0_when_no_transfers_added():
+    counter = TransferOutcomeCounter()
+
+    actual = counter.total
+    expected = 0
+
+    assert actual == expected
+
+
+def test_returns_total_count_by_failure_reason():
+    a_failed_transfer = a_transfer_with_a_final_error()
+    a_non_integrated_transfer = a_transfer_that_was_never_integrated()
+    an_integrated_late_transfer = a_transfer_integrated_beyond_8_days()
+
+    counter = TransferOutcomeCounter()
+    counter.add_transfer(a_failed_transfer)
+    counter.add_transfer(a_non_integrated_transfer)
+    counter.add_transfer(an_integrated_late_transfer)
+    counter.add_transfer(an_integrated_late_transfer)
+    counter.add_transfer(an_integrated_late_transfer)
+
+    actual = counter.count_by_failure_reason(TransferFailureReason.INTEGRATED_LATE)
+    expected = 3
+
+    assert actual == expected
+
+
+def test_returns_total_count_by_failure_reason_0_when_no_transfers_added():
+    counter = TransferOutcomeCounter()
+
+    actual = counter.count_by_failure_reason(TransferFailureReason.INTEGRATED_LATE)
+    expected = 0
 
     assert actual == expected
