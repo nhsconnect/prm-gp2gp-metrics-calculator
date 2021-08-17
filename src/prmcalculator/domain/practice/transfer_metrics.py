d79869c7da3b532b47e44a8fc41578939886e8ea
K-Varsani <kavita.varsani1@nhs.net>
2021-08-17 15:53:49 +0100

    PRMT-1416 [HZ/KV] Added SLA metrics to Transfer Metrics

--- src/prmcalculator/domain/practice/transfer_metrics.py
+++ src/prmcalculator/domain/practice/transfer_metrics.py
@@ -1,6 +1,7 @@
 from collections import Counter
 from typing import Iterable
 
+from prmcalculator.domain.gp2gp.sla import SlaCounter
 from prmcalculator.domain.gp2gp.transfer import (
     Transfer,
     TransferOutcome,
@@ -21,10 +22,13 @@ class TransferMetrics:
     def __init__(self, transfers: Iterable[Transfer]):
         self._counts_by_outcome: Counter[TransferOutcome] = Counter()
         self._counts_by_status: Counter[TransferStatus] = Counter()
+        self._sla_counter: SlaCounter = SlaCounter()
 
         for transfer in transfers:
             self._counts_by_outcome.update([transfer.outcome])
             self._counts_by_status.update([transfer.outcome.status])
+            if transfer.outcome.status == TransferStatus.INTEGRATED_ON_TIME:
+                self._sla_counter.increment(transfer.sla_duration)
 
     def process_failure_not_integrated(self) -> int:
         return self._counts_by_outcome[_NOT_INTEGRATED]
@@ -34,3 +38,12 @@ class TransferMetrics:
             self._counts_by_outcome[_INTEGRATED_LATE]
             + self._counts_by_status[TransferStatus.INTEGRATED_ON_TIME]
         )
+
+    def integrated_within_3_days(self) -> int:
+        return self._sla_counter.within_3_days
+
+    def integrated_within_8_days(self) -> int:
+        return self._sla_counter.within_8_days
+
+    def integrated_beyond_8_days(self) -> int:
+        return self._counts_by_outcome[_INTEGRATED_LATE]
--- tests/unit/domain/practice/test_transfer_metrics.py
+++ tests/unit/domain/practice/test_transfer_metrics.py
@@ -7,6 +7,7 @@ from tests.builders.gp2gp import (
     a_transfer_integrated_between_3_and_8_days,
     a_transfer_integrated_beyond_8_days,
     a_transfer_where_a_copc_triggered_an_error,
+    a_transfer_integrated_within_3_days,
 )
 
 
@@ -40,3 +41,60 @@ def test_returns_integrated_total():
     transfer_metrics = TransferMetrics(transfers=transfers)
 
     assert transfer_metrics.integrated_total() == 2
+
+
+def test_returns_integrated_within_3_days():
+    transfers = [
+        a_transfer_integrated_within_3_days(),
+        a_transfer_integrated_within_3_days(),
+        a_transfer_integrated_within_3_days(),
+        a_transfer_integrated_between_3_and_8_days(),
+        a_transfer_with_a_final_error(),
+        a_transfer_where_the_request_was_never_acknowledged(),
+        a_transfer_where_no_core_ehr_was_sent(),
+        a_transfer_that_was_never_integrated(),
+        a_transfer_integrated_beyond_8_days(),
+        a_transfer_where_a_copc_triggered_an_error(),
+    ]
+
+    transfer_metrics = TransferMetrics(transfers=transfers)
+
+    assert transfer_metrics.integrated_within_3_days() == 3
+
+
+def test_returns_integrated_within_8_days():
+    transfers = [
+        a_transfer_integrated_within_3_days(),
+        a_transfer_integrated_between_3_and_8_days(),
+        a_transfer_integrated_between_3_and_8_days(),
+        a_transfer_with_a_final_error(),
+        a_transfer_where_the_request_was_never_acknowledged(),
+        a_transfer_where_no_core_ehr_was_sent(),
+        a_transfer_that_was_never_integrated(),
+        a_transfer_integrated_beyond_8_days(),
+        a_transfer_where_a_copc_triggered_an_error(),
+    ]
+
+    transfer_metrics = TransferMetrics(transfers=transfers)
+
+    assert transfer_metrics.integrated_within_8_days() == 2
+
+
+def test_returns_integrated_beyond_8_days():
+    transfers = [
+        a_transfer_integrated_within_3_days(),
+        a_transfer_integrated_between_3_and_8_days(),
+        a_transfer_with_a_final_error(),
+        a_transfer_where_the_request_was_never_acknowledged(),
+        a_transfer_where_no_core_ehr_was_sent(),
+        a_transfer_that_was_never_integrated(),
+        a_transfer_integrated_beyond_8_days(),
+        a_transfer_integrated_beyond_8_days(),
+        a_transfer_integrated_beyond_8_days(),
+        a_transfer_integrated_beyond_8_days(),
+        a_transfer_where_a_copc_triggered_an_error(),
+    ]
+
+    transfer_metrics = TransferMetrics(transfers=transfers)
+
+    assert transfer_metrics.integrated_beyond_8_days() == 4
