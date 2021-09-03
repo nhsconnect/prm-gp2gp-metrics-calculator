a604ca003773e1e2c3dd9846a607b58d51e3d255
helen.zhou <helen.zhou1@nhs.net>
2021-09-03 11:50:59 +0100

    PRMT-2320 [HZ] Add to JSON formatter test field containing tuple to ensure can be formatted correctly

--- tests/unit/utils/io/test_json_formatter.py
+++ tests/unit/utils/io/test_json_formatter.py
@@ -13,6 +13,7 @@ def test_json_formatter_correctly_formats_record():
             "module": "a.module",
             "created": 1607965513.358049,
             "extra_field": "some_value",
+            "tuple_field": [(2021, 7), (2021, 6)],
         }
     )
 
@@ -22,6 +23,7 @@ def test_json_formatter_correctly_formats_record():
         "module": "a.module",
         "time": "2020-12-14T17:05:13.358049",
         "extra_field": "some_value",
+        "tuple_field": [[2021, 7], [2021, 6]],
     }
 
     actual_json_string = JsonFormatter().format(record)
