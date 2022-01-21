db54238ccdb55bcee5f921f933132128b5944bee
K-Varsani <kavita.varsani1@nhs.net>
2022-01-21 15:25:22 +0000

    PRMT-2490 [DS/KV] Introduce reporting window which returns a list of days based on given number of months

--- src/prmcalculator/domain/reporting_window.py
+++ src/prmcalculator/domain/reporting_window.py
@@ -2,31 +2,37 @@ from datetime import datetime
 from typing import List, Tuple
 
 from dateutil.relativedelta import relativedelta
-from dateutil.tz import tzutc
+
+from prmcalculator.utils.date_converter import (
+    convert_date_range_to_dates,
+    get_first_day_of_month_datetime,
+)
 
 YearNumber = int
 MonthNumber = int
 YearMonth = Tuple[YearNumber, MonthNumber]
 
 
-class MonthlyReportingWindow:
+class ReportingWindow:
     def __init__(
         self,
         date_anchor_month_start: datetime,
-        metric_monthly_datetimes: List[datetime],
+        dates: List[datetime],
     ):
         self._date_anchor_month_start = date_anchor_month_start
-        self._metric_monthly_datetimes = metric_monthly_datetimes
-        self._latest_metric_month = metric_monthly_datetimes[0]
+        self._dates = dates
+        self._latest_metric_month = get_first_day_of_month_datetime(dates[-1])
 
     @classmethod
     def prior_to(cls, date_anchor: datetime, number_of_months: int):
-        date_anchor_month_start = datetime(date_anchor.year, date_anchor.month, 1, tzinfo=tzutc())
-        metric_monthly_datetimes = [
-            date_anchor_month_start - relativedelta(months=number + 1)
-            for number in range(number_of_months)
-        ]
-        return cls(date_anchor_month_start, metric_monthly_datetimes)
+        date_anchor_month_start = get_first_day_of_month_datetime(date_anchor)
+        first_metric_month_start = date_anchor_month_start - relativedelta(months=number_of_months)
+
+        dates = convert_date_range_to_dates(
+            start_datetime=first_metric_month_start, end_datetime=date_anchor_month_start
+        )
+
+        return cls(date_anchor_month_start, dates)
 
     @property
     def last_metric_month(self) -> YearMonth:
@@ -34,11 +40,8 @@ class MonthlyReportingWindow:
         return month.year, month.month
 
     @property
-    def metric_months(self) -> List[YearMonth]:
-        return [
-            (metric_month.year, metric_month.month)
-            for metric_month in self._metric_monthly_datetimes
-        ]
+    def dates(self) -> List[datetime]:
+        return self._dates
 
     @property
     def date_anchor_month(self) -> YearMonth:
